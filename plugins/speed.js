6bb4049503f3c00a15e30fe5966717ce7bf685df
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-24 21:00:06 +0700

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -1,9 +1,9 @@
 let { performance } = require('perf_hooks')
 let handler = async (m, { conn }) => {
   let old = performance.now()
-  await m.reply('_Testing speed..._')
+  menghuek = await m.reply('_Testing speed..._')
   let neww = performance.now()
-  m.reply(neww - old + 'ms')
+  conn.reply(m.chat, neww - old + 'ms', menghuek)
 }
 handler.help = ['ping', 'speed']
 handler.tags = ['info', 'tools']
