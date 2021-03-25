151f21f1b9bc6ec40499efa31f7817ec8d5e6c81
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-03-25 16:55:37 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,4 +1,4 @@
-let handler = function (m) {
+let handler = async function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
   F = await this.sendContact(m.chat, owner[0], m.getName(owner[0]), m)
   this.reply(m.chat, 'Ini Nomor Owner Ku, Jangan lupa Di save ya >/<', F)
